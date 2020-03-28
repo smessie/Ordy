e01664e6018e90dbef954da5bc304afb2db02ebb
fklinck <francis.klinck@ugent.be>
2020-03-28 23:30:29 +0100

    implement group endpoint logic

--- spring/src/main/kotlin/com/ordy/backend/services/GroupService.kt
+++ spring/src/main/kotlin/com/ordy/backend/services/GroupService.kt
@@ -1,21 +1,55 @@
 package com.ordy.backend.services
 
+import com.ordy.backend.database.models.Group
 import com.ordy.backend.database.models.User
+import com.ordy.backend.database.repositories.GroupRepository
+import com.ordy.backend.exceptions.GenericException
+import com.ordy.backend.exceptions.PropertyException
+import com.ordy.backend.exceptions.ThrowableList
 import com.ordy.backend.wrappers.GroupCreateWrapper
 import com.ordy.backend.wrappers.GroupIdWrapper
 import com.ordy.backend.wrappers.GroupWrapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class GroupService {
+class GroupService(@Autowired val groupRepository: GroupRepository) {
+
+    private val groupNameRegex = Regex("^[A-z0-9 ]+$")
+
+    private fun checkGroupName(name: String, list: ThrowableList) {
+        if (!groupNameRegex.matches(name)) {
+            list.addException(PropertyException(HttpStatus.UNPROCESSABLE_ENTITY, "name", "Name should only contain letters, numbers an spaces"))
+        }
+    }
+
     fun createGroup(user: User, groupWrapper: GroupCreateWrapper) : GroupIdWrapper {
-        //TODO implmement
-        return GroupIdWrapper(0)
+        val throwableList = ThrowableList()
+        checkGroupName(groupWrapper.name, throwableList)
+        throwableList.ifNotEmpty { throw throwableList }
+
+        val group = Group(name = groupWrapper.name, creator = user)
+        groupRepository.save(group)
+        return GroupIdWrapper(group.id)
     }
 
     fun updateGroup(groupId: Int, groupWrapper: GroupCreateWrapper) : GroupWrapper {
-        //TODO
-        return GroupWrapper(0, "YEET")
+        val throwableList = ThrowableList()
+        checkGroupName(groupWrapper.name, throwableList)
+
+        val group: Optional<Group> = groupRepository.findById(groupId)
+        group.ifPresentOrElse(
+                {
+                    group.get().name = groupWrapper.name
+                    groupRepository.save(group.get())
+                },
+                {throwableList.addException(GenericException(HttpStatus.NOT_FOUND, "Group with id $groupId not found"))})
+
+        throwableList.ifNotEmpty { throw throwableList }
+
+        return GroupWrapper(group.get().id, group.get().name)
     }
 
     fun createInvite(groupId: Int, userId: Int) {
