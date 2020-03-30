6f4502037d0bda3845eab80074776310b0d6e06b
maartenvn <maartenvn.eu@gmail.com>
2020-03-30 20:17:57 +0200

    Add default Jackson view to Empty

--- spring/src/main/kotlin/com/ordy/backend/interceptors/AppConfig.kt
+++ spring/src/main/kotlin/com/ordy/backend/interceptors/AppConfig.kt
@@ -1,10 +1,17 @@
 package com.ordy.backend.interceptors
 
-import com.ordy.backend.database.repositories.UserRepository
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ordy.backend.database.View
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.*
+
 
 @Configuration
 class AppConfig: WebMvcConfigurer {
@@ -21,5 +28,22 @@ class AppConfig: WebMvcConfigurer {
                         "/orders/**"
                 ))
                 .excludePathPatterns("/auth/**")
+    }
+
+    @Bean
+    fun jacksonMessageConverter(): MappingJackson2HttpMessageConverter {
+        val converter = MappingJackson2HttpMessageConverter()
+
+        val mapper = ObjectMapper()
+        mapper.setConfig(mapper.serializationConfig.withView(View.Empty::class.java))
+        mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false)
+        converter.objectMapper = mapper
+
+        return converter
+    }
+
+    override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>?>) {
+        converters.add(jacksonMessageConverter());
+        super.configureMessageConverters(converters);
     }
 }
