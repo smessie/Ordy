812b9e0a26249fbebe49225b82ee67c1a3cfebe6
maartenvn <maartenvn.eu@gmail.com>
2020-03-31 17:53:08 +0200

    Add locations endpoint

--- spring/src/main/kotlin/com/ordy/backend/database/repositories/LocationRepository.kt
+++ spring/src/main/kotlin/com/ordy/backend/database/repositories/LocationRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface LocationRepository : JpaRepository<Location, Int> {
     fun findAllByName(s: String) : List<Location>
-
+    fun findFirst30ByNameContaining(s: String): List<Location>
 }
