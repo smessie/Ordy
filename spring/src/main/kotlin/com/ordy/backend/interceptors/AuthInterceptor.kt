b26aef4422309a68ad89595b59d94cda4dea8fff
maartenvn <maartenvn.eu@gmail.com>
2020-04-01 19:32:00 +0200

    Add random value to token to prevent brute-force attacks

--- spring/src/main/kotlin/com/ordy/backend/interceptors/AuthInterceptor.kt
+++ spring/src/main/kotlin/com/ordy/backend/interceptors/AuthInterceptor.kt
@@ -1,12 +1,15 @@
 package com.ordy.backend.interceptors
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.ordy.backend.database.repositories.UserRepository
 import com.ordy.backend.exceptions.GenericException
 import com.ordy.backend.services.TokenService
+import com.ordy.backend.wrappers.TokenWrapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -19,19 +22,25 @@ class AuthInterceptor: HandlerInterceptor{
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, dataObject: Any) : Boolean {
 
         val token = request.getHeader("Authorization") ?: throw GenericException(HttpStatus.UNAUTHORIZED, "Invalid token")
-        val userId = tokenService.decrypt(token).toIntOrNull()
 
-        // Check if decrypted id is numerical
-        if (userId != null) {
-            val optionalUser = userRepo.findById(userId)
+        try {
+            val tokenDecrypt = jacksonObjectMapper().readValue(tokenService.decrypt(token), TokenWrapper::class.java)
+            val userId = tokenDecrypt.userId
 
-            if (optionalUser.isPresent) {
-                request.setAttribute("userId", optionalUser.get().id)
-                return true
+            // Check if decrypted id is numerical
+            if (userId != null) {
+                val optionalUser = userRepo.findById(userId)
+
+                if (optionalUser.isPresent) {
+                    request.setAttribute("userId", optionalUser.get().id)
+                    return true
+                } else {
+                    throw GenericException(HttpStatus.UNAUTHORIZED, "Invalid token")
+                }
             } else {
                 throw GenericException(HttpStatus.UNAUTHORIZED, "Invalid token")
             }
-        } else {
+        } catch (exception: Exception) {
             throw GenericException(HttpStatus.UNAUTHORIZED, "Invalid token")
         }
     }
