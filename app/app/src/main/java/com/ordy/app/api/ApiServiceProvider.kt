e96a229b10ff8c3ab7df113607efad6f52c8debc
maartenvn <maartenvn.eu@gmail.com>
2020-04-02 14:58:31 +0200

    Better rendering of order card

--- app/app/src/main/java/com/ordy/app/api/ApiServiceProvider.kt
+++ app/app/src/main/java/com/ordy/app/api/ApiServiceProvider.kt
@@ -35,6 +35,7 @@ class ApiServiceProvider {
             .addConverterFactory(GsonConverterFactory.create(gson))
             .client(client)
             .baseUrl("http://dev.api.ordy.ga")
+            .baseUrl("http://192.168.0.151:8080")
             .build()
 
         return retrofit.create(ApiService::class.java)
