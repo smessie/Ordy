6fa979fa1657c8ff8734a935868e8be6f05a950a
iesmessa <Ieben.Smessaert@UGent.be>
2020-04-12 19:12:31 +0200

    Refactor payments to use repository

--- app/app/src/main/java/com/ordy/app/ui/payments/PaymentsFragment.kt
+++ app/app/src/main/java/com/ordy/app/ui/payments/PaymentsFragment.kt
@@ -6,14 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.ordy.app.R
-import com.ordy.app.api.ApiServiceViewModelFactory
+import com.ordy.app.api.RepositoryViewModelFactory
 import com.ordy.app.databinding.FragmentPaymentsBinding
 
 class PaymentsFragment : Fragment() {
 
-    private val viewModel: PaymentsViewModel by activityViewModels { ApiServiceViewModelFactory(requireContext()) }
+    private val viewModel: PaymentsViewModel by activityViewModels {
+        RepositoryViewModelFactory(
+            requireContext()
+        )
+    }
 
     /**
      * Called when view is created.
