2a508640819da9f2c290735a7844e34ec3198545
iesmessa <Ieben.Smessaert@UGent.be>
2020-04-12 19:25:48 +0200

    Refactor settings to use repository

--- app/app/src/main/java/com/ordy/app/ui/settings/SettingsActivity.kt
+++ app/app/src/main/java/com/ordy/app/ui/settings/SettingsActivity.kt
@@ -5,18 +5,23 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ordy.app.R
-import com.ordy.app.api.ApiServiceViewModelFactory
+import com.ordy.app.api.RepositoryViewModelFactory
 import com.ordy.app.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
 
-    private val viewModel: SettingsViewModel by viewModels { ApiServiceViewModelFactory(applicationContext) }
+    private val viewModel: SettingsViewModel by viewModels {
+        RepositoryViewModelFactory(
+            applicationContext
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Create binding for the activity.
-        val binding: ActivitySettingsBinding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
+        val binding: ActivitySettingsBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_settings)
         binding.handlers = SettingsHandlers(this, viewModel)
     }
 }
