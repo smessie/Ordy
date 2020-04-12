68110b4f709b2b1219e34e7e4a8be650a718707d
maartenvn <maartenvn.eu@gmail.com>
2020-04-12 21:56:47 +0200

    Fix back button behaviour when login screen is opened

--- app/app/src/main/java/com/ordy/app/MainActivity.kt
+++ app/app/src/main/java/com/ordy/app/MainActivity.kt
@@ -30,9 +30,14 @@ class MainActivity : AppCompatActivity() {
         // If not, open the login activity
         if (accessToken == null || accessToken.isBlank()) {
             val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
 
-            return;
+            // Reset the back stack.
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+
+            startActivity(intent)
+            finish()
+
+            return
         }
 
         // Set the activity layout.
@@ -146,7 +151,13 @@ class MainActivity : AppCompatActivity() {
 
             // Go to the login activity
             val intent = Intent(this, LoginActivity::class.java)
+
+            // Reset the back stack.
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+
             startActivity(intent)
+
+            finish()
 
             true
         }
