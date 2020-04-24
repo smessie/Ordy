66927878a6a31b916617126485b96f6d21e26ed7
maartenvn <maartenvn.eu@gmail.com>
2020-04-24 20:44:44 +0200

    Add ability to view a bill image (placeholder)

--- app/app/src/main/java/com/ordy/app/ui/orders/overview/OverviewOrderHandlers.kt
+++ app/app/src/main/java/com/ordy/app/ui/orders/overview/OverviewOrderHandlers.kt
@@ -1,6 +1,23 @@
 package com.ordy.app.ui.orders.overview
 
+import androidx.fragment.app.DialogFragment
+import com.ordy.app.R
+import com.ordy.app.ui.orders.overview.viewbill.ViewBillDialog
+
 class OverviewOrderHandlers(
     val activity: OverviewOrderActivity,
     val viewModel: OverviewOrderViewModel
-)
+) {
+
+    /**
+     * Show the bill image for a specific order.
+     */
+    fun showBill() {
+        // Open the pick location dialog.
+        val fragmentManager = activity.supportFragmentManager
+
+        val dialog = ViewBillDialog()
+        dialog.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.AppTheme_NoActionBar)
+        dialog.show(fragmentManager, "dialog")
+    }
+}
--- app/app/src/main/java/com/ordy/app/ui/orders/overview/viewbill/ViewBillDialog.kt
+++ app/app/src/main/java/com/ordy/app/ui/orders/overview/viewbill/ViewBillDialog.kt
@@ -1,2 +1,63 @@
 package com.ordy.app.ui.orders.overview.viewbill
 
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import com.github.chrisbanes.photoview.PhotoView
+import com.ordy.app.R
+import com.ordy.app.databinding.DialogViewBillBinding
+import com.ordy.app.ui.orders.overview.OverviewOrderViewModel
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.dialog_view_bill.view.*
+import java.lang.Exception
+
+
+class ViewBillDialog : DialogFragment() {
+
+    private val viewModel: OverviewOrderViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        inflater.inflate(R.layout.dialog_view_bill, container, false)
+
+        // Create binding for the fragment.
+        val binding = DialogViewBillBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+
+        // Setup the toolbar
+        val toolbar: Toolbar = binding.root.findViewById(R.id.toolbar)
+        toolbar.title = "View bill: ${viewModel.getOrder().requireData().location.name}"
+        toolbar.setNavigationOnClickListener { dismiss() }
+
+        val photoView = binding.root.findViewById(R.id.bill_image) as PhotoView
+
+        Picasso.get()
+            .load("https://media-cdn.tripadvisor.com/media/photo-s/0e/14/ff/95/rekening.jpg")
+            .into(photoView, object : Callback {
+
+                override fun onSuccess() {
+                    binding.root.bill_image_loading.visibility = View.INVISIBLE
+                }
+
+                override fun onError(e: Exception?) {
+                    binding.root.bill_image_loading.visibility = View.INVISIBLE
+                    binding.root.bill_image_error.visibility = View.VISIBLE
+                }
+            })
+
+
+
+        return binding.root
+    }
+}
