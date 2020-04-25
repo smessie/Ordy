3c4216ed7a915b2c74bbc526e2d481c742c32a12
maartenvn <maartenvn.eu@gmail.com>
2020-04-25 16:59:48 +0200

    Add tests for 'Query' class

--- app/app/src/test/java/com/ordy/app/api/QueryTest.kt
+++ app/app/src/test/java/com/ordy/app/api/QueryTest.kt
@@ -1,4 +1,47 @@
 package com.ordy.app.api
 
+import com.ordy.app.api.util.Query
+import com.ordy.app.api.util.QueryStatus
+import org.junit.Assert
+import org.junit.Assert.fail
+import org.junit.Test
+import java.lang.IllegalStateException
+
+
 class QueryTest {
+
+    @Test
+    fun `Query initial status must be INITIALIZED`() {
+        val query: Query<String> = Query()
+
+        Assert.assertEquals(query.status, QueryStatus.INITIALIZED)
+    }
+
+    @Test
+    fun `requireData must throw an error when data is null`() {
+        val query: Query<String> = Query()
+        query.data = null
+
+        try {
+            query.requireData()
+
+            fail("Expected an IllegalStateException to be thrown")
+        } catch (exception: IllegalStateException) {
+            Assert.assertEquals(exception.message, "Data is not present on Query object")
+        }
+    }
+
+    @Test
+    fun `requireError must throw an error when error is null`() {
+        val query: Query<String> = Query()
+        query.error = null
+
+        try {
+            query.requireError()
+
+            fail("Expected an IllegalStateException to be thrown")
+        } catch (exception: IllegalStateException) {
+            Assert.assertEquals(exception.message, "Error is not present on Query object")
+        }
+    }
 }
