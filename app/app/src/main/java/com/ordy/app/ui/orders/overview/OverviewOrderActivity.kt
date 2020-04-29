2387b60e199e20951b8fbfa0374e5b0f5ec1266e
maartenvn <maartenvn.eu@gmail.com>
2020-04-29 09:51:32 +0200

    Fix some strange behaviour when refreshing

--- app/app/src/main/java/com/ordy/app/ui/orders/overview/OverviewOrderActivity.kt
+++ app/app/src/main/java/com/ordy/app/ui/orders/overview/OverviewOrderActivity.kt
@@ -77,26 +77,6 @@ class OverviewOrderActivity : AppCompatActivity() {
         // Fetch the specific order.
         viewModel.refreshOrder(orderId)
 
-        // Register the swipe to refresh listener on every tab that may have it.
-        for (i in 0 until tabsAdapter.count) {
-            val currentView = tabsAdapter.getItem(i).view
-            val refreshView = currentView?.findViewById<SwipeRefreshLayout>(R.id.order_refresh)
-
-            if(refreshView != null) {
-                // Swipe to refresh
-                refreshView.setOnRefreshListener {
-                    viewModel.refreshOrder(orderId)
-                }
-
-                // Stop refreshing on load
-                viewModel.getOrderMLD().observe(this, Observer {
-                    if (it.status == QueryStatus.SUCCESS || it.status == QueryStatus.ERROR) {
-                        refreshView.isRefreshing = false
-                    }
-                })
-            }
-        }
-
         // Observe the changes of the fetch.
         viewModel.getOrderMLD().observe(this, Observer {
 
