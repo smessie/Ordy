7a09cb1101f3d23e8e4703c23d53b36b98a74d9d
iesmessa <Ieben.Smessaert@UGent.be>
2020-05-07 17:18:22 +0200

    Write Register UI test (#87)

---
+++ app/app/src/androidTest/java/com/ordy/app/ui/RegisterTest.kt
@@ -0,0 +1,97 @@
+package com.ordy.app.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.javafaker.Faker
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.ordy.app.R
+import com.ordy.app.api.Repository
+import com.ordy.app.api.util.Query
+import com.ordy.app.ui.login.LoginActivity
+import com.ordy.app.ui.login.LoginViewModel
+import okhttp3.ResponseBody
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import org.koin.test.mock.MockProviderRule
+import org.koin.test.mock.declareMock
+import org.mockito.Mockito
+
+@RunWith(AndroidJUnit4::class)
+class RegisterTest : KoinTest {
+
+    /**
+     * Java faker for faking data.
+     */
+    private val faker = Faker()
+
+    /**
+     * ViewModel that has been created using Koin injection.
+     */
+    private val mockLoginViewModel: LoginViewModel by inject()
+
+    /**
+     * Repository that has been created using Koin injection.
+     */
+    private val mockRepository: Repository by inject()
+
+    /**
+     * Mock provider for Koin testing.
+     */
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz ->
+        Mockito.mock(clazz.java)
+    }
+
+    @Before
+    fun setup() {
+        declareMock<LoginViewModel>()
+        declareMock<Repository>()
+    }
+
+    /**
+     * Test the input fields and the register button.
+     * When typing register credentials in appropriate fields and hitting the register button,
+     * the register function should be called with the same register credentials.
+     */
+    @Test
+    fun onRegisterButtonClickShouldRegister() {
+        val username = faker.name().username()
+        val email = faker.internet().emailAddress()
+        val password = faker.internet().password()
+
+        val registerMLD = MutableLiveData(Query<ResponseBody>())
+
+        whenever(mockLoginViewModel.getIsLoginMLD()).thenReturn(MutableLiveData(false))
+        whenever(mockRepository.register(registerMLD, username, email, password)).then { }
+        whenever(mockLoginViewModel.getRegisterMLD()).thenReturn(registerMLD)
+
+        ActivityScenario.launch(LoginActivity::class.java)
+
+        onView(withId(R.id.input_register_username_text)).perform(
+            typeText(username),
+            closeSoftKeyboard()
+        )
+        onView(withId(R.id.input_register_email_text)).perform(typeText(email), closeSoftKeyboard())
+        onView(withId(R.id.input_register_password_text)).perform(
+            typeText(password),
+            closeSoftKeyboard()
+        )
+        onView(withId(R.id.input_register_password_repeat_text)).perform(
+            typeText(password),
+            closeSoftKeyboard()
+        )
+
+        onView(withId(R.id.button_login)).perform(click())
+
+        verify(mockLoginViewModel).register(username, email, password)
+    }
+}
